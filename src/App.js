2dc0a0967c2a8d8e5eab3f40dc4a1097d9a7a897
Bradley Griffin <bigriff11@gmail.com>
2023-04-06 13:27:51 -0600

    Reset playground

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Home from "./Home";
-import Create from "./Create";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import BlogDetails from "./BlogDetails";
-import NotFound from "./NotFound";
-
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route exact path={"/"}>
-              <Home />
-            </Route>
-            <Route path={"/create"}>
-              <Create />
-            </Route>
-            <Route path={"/blogs/:id"}>
-              <BlogDetails />
-            </Route>
-            <Route path={"*"}>
-              <NotFound />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
